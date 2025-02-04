ac8a92cd6eae9af42a831d0564d1898a0f515bf2
twennb <tom.wennberg@hotmail.com>
2025-02-04 10:40:03 +0100

    Experimenting with variable types, difference between primitive and reference types.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,41 @@
+import java.awt.*;
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        // primitive types
+        int age = 30;
+        byte ageByte = 30, temperature = 20; //don't do this
+        ageByte = 35;
+
+        byte myAge = 30;
+        byte herAge = myAge;
+        System.out.println(herAge + ageByte);
+
+        int viewsCount = 123_456_789;
+        long viewsCountLong = 3_123_456_789L;
+        double price = 11.57;
+        float priceFloat = 10.99f;
+        char letter = 'A';
+        String letters = "ABC";
+        boolean isEligible = false;
+
+        // reference types
+        Date now = new Date();
+        System.out.println(now);
+
+        // --- primitives store values, reference types store references to values. independent|dependent
+        byte x = 1;
+        byte y = x;
+        x = 2;
+        System.out.println(y);
+
+        Point point1 = new Point(1, 1);
+        Point point2 = point1;
+        point1.x = 4;
+        System.out.println(point2);
     }
 }
