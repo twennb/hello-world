c93664a9601e27f9a11db9d9707cd996fe5f0141
twennb <tom.wennberg@hotmail.com>
2025-02-06 13:21:13 +0100

    Basic multi-dimensional array example

--- src/Practice.java
+++ src/Practice.java
@@ -11,7 +11,21 @@ public class Practice {
         System.out.println(numbers);
         System.out.println(Arrays.toString(numbers));
 
+        //can also assign values right away by using {}
         int[] numbersQuick = {1,2,3,6,8,9,4};
+        //several methods can be accessed through the
+        //"." operator for lots of flexibility:
         System.out.println(numbersQuick.length);
+        //Arrays.sort(); can rearrange an unsorted array
+
+
+        //arrays can be multi-dimensional to represent
+        //matrices or cubes:
+        int[][] matrix = new int[2][3];
+        matrix[0][0] = 1;
+        matrix[0][1] = 2;
+        //to print out a multi-dimensional array we use
+        //Arrays.deepToString()
+        System.out.println(Arrays.deepToString(matrix));
     }
 }
